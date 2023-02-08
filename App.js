e04e036b660daf50e990835cd767d5f53b270f80
cleomaster <cleo.master0000@gmail.com>
2023-02-08 10:41:15 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import AdmissionScreenPersonalDetails from './app/screens/AdmissionScreenPersonalDetails';
+import AppNavigator from './app/AppNavigator/AppNavigator';
+
+
+export default function App() {
+  return (
+   <AppNavigator />
+  );
+}
+
